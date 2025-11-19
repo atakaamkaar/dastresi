3fdf11ec427530084d7483125d5752c2316791a3
atakaamkaar <drkaamkaar76@gmail.com>
2025-11-19 13:07:36 +0330

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,23 @@ import LatestArticles from "./components/LatestArticles/LatestArticles";
 import Footer from "./components/Footer/Footer";
 import WhyDastresi from "./components/WhyDastresi/WhyDastresi";
 import "../src/assets/css/stylesheet.css"
+
+
 function App() {
   return (
-   <div className="App">
-    <Header/>
-    <SwiperFile/>
-    <DailyDiscount/>
-    <ChosenCategory/>
-    <AvailableNow/>
-    <WhyDastresi/>
-    <BestSellers/>
-    <MehboobBrands/>
-    <LatestArticles/>
-    <Footer/>
-   </div>
+    <div className="App">
+      <Header />
+      <SwiperFile />
+      <DailyDiscount />
+      <ChosenCategory />
+      <AvailableNow />
+      <WhyDastresi />
+      <BestSellers />
+      <MehboobBrands />
+      <LatestArticles />
+      <Footer />
+    
+    </div>
   );
 }
 
